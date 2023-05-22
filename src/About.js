f6cbe0a4c299a7697c9fe7489655e6bf871e1df9
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-05-22 18:26:29 -0500

    Really done this time

--- src/About.js
+++ src/About.js
@@ -5,7 +5,7 @@ export default function About() {
     return (
         /* TODO: render information about the developers */
         <div>
-            <p>Conner Kit: A young software developer who is eager to learn more!</p>
+            <p>Conner Thompson: A young software developer who is eager to learn more!</p>
             <p>Jared Plummer: A young software developer who is eager to learn more!</p>
 
         </div>
