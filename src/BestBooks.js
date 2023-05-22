2fa1aa44c1242cc0243f5fb4125cb5898908a0a6
Jared E Plummer <113934345+JaredPlummer5@users.noreply.github.com>
2023-05-22 18:22:55 -0500

    Lab 11 done

--- src/BestBooks.js
+++ src/BestBooks.js
@@ -1,10 +1,19 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+import BookCarousel from './BookCarousel';
+
 
 function BestBooks() {
     const [books, setBooks] = useState([]);
 
     useEffect(() => {
         // TODO: Make a GET request to your API to fetch all the books from the database
+        let url = 'http://localhost:3001/books'
+        let response = axios.get(url)
+            .then(res => {
+                setBooks(res.data)
+console.log(res)
+            })
         // Update the books state with the fetched data
     }, []);
 
@@ -14,11 +23,16 @@ function BestBooks() {
         <>
             <h2>My Essential Lifelong Learning &amp; Formation Shelf</h2>
 
-            {books.length ? (
+            {books.length !== 0 ? (
+             
+                    <BookCarousel books={books} />
+           
+            ) : (<div>
+                <h3>No Books Found :(</h3 >
                 <p>Book Carousel coming soon</p>
-            ) : (
-                <h3>No Books Found :(</h3>
-            )}
+            </div>
+            )
+            }
         </>
     );
 }
--- src/Footer.js
+++ src/Footer.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Navbar from 'react-bootstrap/Navbar';
 
-import React from 'react'
 
 export default function Footer() {
     return (
